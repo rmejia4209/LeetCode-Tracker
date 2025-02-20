65de51c081d85d61e59cb18845389f6b042319f5
Rigo Mejia <rmejia4209@gmail.com>
2025-02-20 16:59:28 -0600

    Example of list movement animation

--- leetcode-tracker/app/page.tsx
+++ leetcode-tracker/app/page.tsx
@@ -1,10 +1,60 @@
 
+"use client"
 
-export default function Home() {
+import * as motion from "motion/react-client"
+import { useEffect, useState } from "react"
 
-  return (
+
+/**
+ * ==============   Utils   ================
+ */
+function shuffle(array: string[]) {
+    return array.sort(() => Math.random() - 0.5)
+}
+
+
+function Home() {
+
+  /*
     <div className="flex items-center">
       Home Page
-    </div>
-  );
+    </div>          transition={{
+            type: "spring",
+            damping: 150,
+            stiffness: 2000,
+          }}
+  */
+
+
+  const [order, setOrder] = useState([
+    'Roxie', 'Puma', 'Vino', 'Diamond', 'Pet Rock', 'Murphy', 'Kiki', 'Keke'
+  ])
+
+  useEffect(() => {
+      const timeout = setTimeout(() => setOrder(shuffle([...order])), 5000)
+      return () => clearTimeout(timeout)
+  }, [order])
+
+  return (
+    <table className="table table-pin-rows border-8 h-fit">
+    <thead>
+      <tr>
+        <th>Sticky</th>
+      </tr>
+    </thead>
+    <tbody>      
+      {order.map((word) => (
+        <motion.tr
+          key={word}
+          layout
+          transition={{ type: "tween", duration: 0.25, ease: "easeInOut" }}
+
+        ><td>{word}</td></motion.tr>
+      ))}
+    </tbody>     
+
+    </table>
+  )
 }
+
+export default Home;
