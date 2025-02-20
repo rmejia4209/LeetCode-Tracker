a7bde317c14c5e3086574644adfa746af5ab3f8d
Rigo Mejia <rmejia4209@gmail.com>
2025-02-20 17:08:38 -0600

    Removed example of table layout animation

--- leetcode-tracker/app/page.tsx
+++ leetcode-tracker/app/page.tsx
@@ -1,59 +1,10 @@
 
-"use client"
-
-import * as motion from "motion/react-client"
-import { useEffect, useState } from "react"
-
-
-/**
- * ==============   Utils   ================
- */
-function shuffle(array: string[]) {
-    return array.sort(() => Math.random() - 0.5)
-}
-
 
 function Home() {
-
-  /*
+  return (
     <div className="flex items-center">
       Home Page
-    </div>          transition={{
-            type: "spring",
-            damping: 150,
-            stiffness: 2000,
-          }}
-  */
-
-
-  const [order, setOrder] = useState([
-    'Roxie', 'Puma', 'Vino', 'Diamond', 'Pet Rock', 'Murphy', 'Kiki', 'Keke'
-  ])
-
-  useEffect(() => {
-      const timeout = setTimeout(() => setOrder(shuffle([...order])), 5000)
-      return () => clearTimeout(timeout)
-  }, [order])
-
-  return (
-    <table className="table table-pin-rows border-8 h-fit">
-    <thead>
-      <tr>
-        <th>Sticky</th>
-      </tr>
-    </thead>
-    <tbody>      
-      {order.map((word) => (
-        <motion.tr
-          key={word}
-          layout
-          transition={{ type: "tween", duration: 0.25, ease: "easeInOut" }}
-
-        ><td>{word}</td></motion.tr>
-      ))}
-    </tbody>     
-
-    </table>
+    </div>
   )
 }
 
