bd5f08fbee669b73118919fab1103afd5a7e8f22
Rigo Mejia <rmejia4209@gmail.com>
2025-02-20 16:57:38 -0600

    Added chevron icon. Wrap in div and rotate to change direction

---
+++ leetcode-tracker/app/_components/icons/Chevron.tsx
@@ -0,0 +1,22 @@
+
+
+
+function Chevron() {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        className="size-4 stroke-1 "
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="m4.5 15.75 7.5-7.5 7.5 7.5" 
+        />
+      </svg>
+    )
+  }
+  
+  export default Chevron;
