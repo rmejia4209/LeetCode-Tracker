cd36dfff2acab004ccae6300492c035190f02359
Rigo Mejia <rmejia4209@gmail.com>
2025-02-24 21:55:57 -0600

    Created form that allows user to input time in h:m:s format and provide a solution rating

---
+++ leetcode-tracker/app/_components/forms/ProblemAttemptForm.tsx
@@ -0,0 +1,41 @@
+"use client"
+import { useState, useEffect, useActionState } from "react";
+import TimeInput from "../inputs/TimeInput";
+import AttemptRating from "../inputs/AttemptRating";
+import ErrorAlert from "../feedback/CompactErrorAlert";
+import { postAttempt } from "@/app/_actions/actions";
+
+function ProblemAttemptForm() {
+  const [error, formAction, isPending] = useActionState(postAttempt, null);
+  const [showError, setShowError] = useState(false);
+
+  useEffect(() => {
+    if (error) setShowError(true)
+  }, [error])
+
+  return (
+
+    <form
+      action={formAction}
+      className="flex flex-col gap-2"
+    >
+      <input type="hidden" name="problemId" value="1"/>
+      <TimeInput
+        problemId={1}
+        inErrorState={showError}
+        resetErrorState={() => {if (showError) setShowError(false);}}
+      />
+      <AttemptRating/>
+      <button disabled={isPending} className="btn btn-sm btn-outline btn-primary">
+        {
+          isPending 
+          ? <span className="loading loading-dots loading-sm"></span> 
+          : "submit"
+        }
+      </button>
+      <ErrorAlert isVisible={showError} msg={error?.message}/>
+    </form>
+  )
+}
+
+export default ProblemAttemptForm;
